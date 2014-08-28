bbe37b4c31d92d8e07bbefc6fcb8577aa0c89b57
Blade Barringer <blade@crookedneighbor.com>
2014-08-28 11:23:37 -0500

    Rewrote tests

--- test/upvoting.js
+++ test/upvoting.js
@@ -13,37 +13,40 @@ chai.use(sinonChai);
 describe('Upvoting', function () {
    describe('detectUpvote()', function () {
       var usernames = ['alice', 'bob', 'charlie'];
+      var regex = helpers.usersRegex(usernames);
+
       it('detects username followed by a "+"', function () {
-        var message = {content:'bob+'};
-        expect(helpers.detectUpvoteRegex(usernames)).to.be.eq('bob');
+        var message = 'bob+';
+        expect(helpers.matchUser(message, regex)).to.be.eq('bob');
       });
       it('detecs "username:" followed by a "+" ', function () {
-        var message = {content:'bob:+'};
-        expect(helpers.detectUpvoteRegex(usernames)).to.be.eq('bob');
+        var message = 'bob:+';
+        expect(helpers.matchUser(message, regex)).to.be.eq('bob');
       });
       it('detecs "username:", space character followed by a "+" ', function(){
-        var message = {content:'bob	:+'};
-        expect(helpers.detectUpvoteRegex(usernames)).to.be.eq('bob');
+        var message = 'bob	:+';
+        expect(helpers.matchUser(message, regex)).to.be.eq('bob');
       });
       it('doesnt detect if there is anything between the username and "+"', function() {
-        var message = {content:'bob	foo:+'};
-        expect(helpers.detectUpvoteRegex(usernames)).to.not.be.ok;
+        var message = 'bob	foo:+';
+        expect(helpers.matchUser(message, regex)).to.not.be.ok;
       });
       it('detects if username starts with space', function () {
-        var message = {content:'things   bob	:+'};
-        expect(helpers.detectUpvoteRegex(usernames)).to.be.eq('bob');
+        var message = 'things   bob	:+';
+        expect(helpers.matchUser(message, regex)).to.be.eq('bob');
       });
       it('detects if username doesnt start with space', function() {
-        var message = {content:'thingbob	:+'};
-        expect(helpers.detectUpvoteRegex(usernames)).to.not.be.ok;
+        var message = 'thingbob	:+';
+        expect(helpers.matchUser(message, regex)).to.not.be.ok;
       });
       it('detects with space after ":" and before "+"', function () {
-        var message = {content:'bob:  +'};
-        expect(helpers.detectUpvoteRegex(usernames)).to.be.eq('bob');
+        var message = 'bob:  +';
+        expect(helpers.matchUser(message, regex)).to.be.eq('bob');
       });
    });
    describe('emit upvote event', function () {
      // Since we dont really want to setup the irc module for a test
+      var usernames = ['alice', 'bob', 'charlie'];
       Haxfred.irc = {}
       Haxfred.irc.users = function() {
         return usernames;
